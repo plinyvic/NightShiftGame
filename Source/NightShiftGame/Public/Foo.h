64222db6db2763096dd064beb2babf2880161140
plinyvic <plinyvicgames@gmail.com>
2023-10-17 23:09:45 -0400

    generates initial project source files

---
+++ Source/NightShiftGame/Public/Foo.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ *  exists solely to provide source for project file generation
+ */
+class NIGHTSHIFTGAME_API Foo
+{
+public:
+	Foo();
+	~Foo();
+};
