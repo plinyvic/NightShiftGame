cc51e2f1e494d1c06d2567381dec1ba60484207c
plinyvic <plinyvicgames@gmail.com>
2023-10-26 01:01:56 -0400

    adds script to delete binaries when changing branch
    RESETPROJECTFILESSCRIPT.bat deletes the Binaries, DerivedDataCache, Intermediate, and Saved directories, as well as visual studio files.
    should make changing branches easier as it will force a recompile of the game with current branch files and prevent files from another branch causing a dirty state.

---
+++ RESETPROJECTFILES.bat
@@ -0,0 +1,14 @@
+del /s /q Binaries
+rmdir /s /q Binaries
+
+del /s /q DerivedDataCache
+rmdir /s /q DerivedDataCache
+
+del /s /q Intermediate
+rmdir /s /q Intermediate
+
+del /s /q Saved
+rmdir /s /q Saved
+
+del /q NightShiftGame.sln
+del /q .vsconfig
